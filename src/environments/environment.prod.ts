a76e8eb3f4246f89a1bb81f46ccd6d798e3f97af
Nico <ncfortuin27@gmail.com>
2020-09-13 13:41:46 +0200

    keys deleted

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,4 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: "AIzaSyDh-NTL96frMkzPszwbrcf0hdQ8pRA67I0",
-    authDomain: "foodparcel-3f789.firebaseapp.com",
-    databaseURL: "https://foodparcel-3f789.firebaseio.com",
-    projectId: "foodparcel-3f789",
-    storageBucket: "foodparcel-3f789.appspot.com",
-    messagingSenderId: "482821929466",
-    appId: "1:482821929466:web:0f574564e285fd8526cc43",
-  },
+  firebase: {},
 };
